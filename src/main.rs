df41c4bfcc35e3253e7c48dbc28146dd718f0b3e
LuckyZacha <zachatatem@gmail.com>
2022-12-26 17:05:51 -0500

    Feat: loop and tuple

--- src/main.rs
+++ src/main.rs
@@ -31,4 +31,26 @@ fn main() {
         
    } 
 
+   let arr = [1, 2, 3, 4, 5];
+   let mut loop_id = 0;
+   loop {
+    if arr[loop_id] % 2 == 0 {
+        loop_id += 1;
+        continue
+    }
+    if arr[loop_id] == 5 {
+        break;
+    }
+    println!("val : {}", arr[loop_id]);
+    loop_id += 1;
+   }
+
+   for val in arr.iter() {
+    println!("val: {}", val);
+   }
+
+   let tup: (u8, String, f64) = (47, "DO".to_string(), 50_000.0);
+   println!("NAME:{}", tup.1);
+   let (v1, v2, v3) = tup;
+   println!("Age:{}", v1);
 }
