c2e3b6388cc9645d216f39062999992918c5de90
Raymond <magicpalmtree.space@gmail.com>
2022-12-26 19:13:20 -0500

    Feat: string s and enumvs and vectors

--- src/main.rs
+++ src/main.rs
@@ -7,50 +7,74 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    let mut my_age = 47;
-    let voting_age = 18;
-    let can_vote = if my_age >= 18 {
-        true
-    } else {
-        false
-    };
-    println!("{}", can_vote);
-
-    let age2 = 8;
-    match age2  {
-        1..=18 => println!("Imp"),
-        21 | 50 => println!("Imp"),
-        65..=i32::MAX => println!("Imp"),
-        _ => println!("Not imp")
-    };
-
-   match my_age.cmp(&voting_age) {
-        Ordering::Less => println!("Can't vote"),
-        Ordering::Greater => println!("Can't vote"),
-        Ordering::Equal => println!("Can't vote"),
-        
-   } 
-
-   let arr = [1, 2, 3, 4, 5];
-   let mut loop_id = 0;
-   loop {
-    if arr[loop_id] % 2 == 0 {
-        loop_id += 1;
-        continue
+    let mut str = String::new();
+    str.push('A');
+    str.push_str(" Word");
+    for word in str.split_whitespace() {
+        println!("{}", word);
     }
-    if arr[loop_id] == 5 {
-        break;
+
+    let st2 = str.replace("A", "Another");
+    println!("{}", st2);
+
+    let st3 = String::from("x r t b h k k a m c");
+    let mut v1: Vec<char> = st3.chars().collect();
+    v1.sort();
+    v1.dedup();
+    for char in v1 {
+        println!("{}", char);
     }
-    println!("val : {}", arr[loop_id]);
-    loop_id += 1;
-   }
+    let st4 = "Random string";
+    let mut st5 = st4.to_string();
+    println!("{}", st5);
 
-   for val in arr.iter() {
-    println!("val: {}", val);
-   }
+    let byte_arr1 = st5.as_bytes();
+    let st6 = &st5[0..6];
+    println!("{}", st6.len());
+    st5.clear();
 
-   let tup: (u8, String, f64) = (47, "DO".to_string(), 50_000.0);
-   println!("NAME:{}", tup.1);
-   let (v1, v2, v3) = tup;
-   println!("Age:{}", v1);
+
+    let st6 = String::from("Just some");
+    let st7 = String::from("ss");
+    let st8 = st6 + &st7;
+    println!("{}", st8);
+
+    // use as for cast
+
+    enum Days {
+        Monday,
+        Saturday,
+        Sunday
+    }
+
+    impl Days {
+        fn is_weekend(&self) -> bool {
+            match self {
+                Days::Saturday | Days::Sunday => true,
+                _ => false
+            }
+
+        }
+    }
+
+
+    let vec1: Vec<i32> = Vec :: new();
+    let mut vec2 = vec![1, 2, 3, 4];
+    vec2.push(5);
+    let second: &i32 = &vec2[1];
+    match vec2.get(1) {
+        Some(second) => println!("{}", second),
+        None => print!("SDFDF"),
+    }
+
+    for i in &mut vec2 {
+        *i *= 2;        
+    }
+
+    for i in &vec2{
+        println!("{}", i);
+    }
+    println!("Vec{}", vec2.len());
+    println!("Pop : {:?}", vec2.pop());
+
 }
